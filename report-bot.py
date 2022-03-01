34e220586adf410b54e9fbc7c478dc218011e3c9
Axelxs <88009984+Axelxs@users.noreply.github.com>
2022-02-28 23:39:34 -0600

    Create report-bot.py

---
+++ report-bot.py
@@ -0,0 +1,23 @@
+from time import sleep
+print("\033[;36m")
+print("TIKTOK REPORT BOT")
+print("┈┈┈╱▔▔▔▔▔▔╲┈╭━━━╮┈┈")
+print("┈┈▕┈╭━╮╭━╮┈▏┃BOO.┃┈┈")
+print("┈┈▕┈┃╭╯╰╮┃┈▏╰┳━━╯┈┈")
+print("┈┈▕┈╰╯╭╮╰╯┈▏┈┃┈┈┈┈┈")
+print("┈┈▕┈┈┈┃┃┈┈┈▏━╯┈┈┈┈┈")
+print("┈┈▕┈┈┈╰╯┈┈┈▏┈┈┈┈┈┈┈")
+print("┈┈▕╱╲╱╲╱╲╱╲▏┈┈┈┈┈┈┈")
+sleep(1)
+print ("///////////////////////////////////////////")
+print (" TikTok: @p.a.s.t.r.y.b.o.y")
+print ("///////////////////////////////////////////")
+sleep(1)
+username = ""
+username = input("username: @")
+print("\033[;32m")
+b = 1
+while b < 9999999999999999999999:
+    sleep(1)
+    print (("[*]"),"REPORT-BOT FOR TIKTOK USER", (username), (b))
+    b = b +1
